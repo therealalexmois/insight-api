834c0b7c6c6245121ff1df2ea452f002ba40d02d
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-23 14:15:50 +0300

    fix(loggin): move contextvar into top of processors

--- src/app/logging/logging.py
+++ src/app/logging/logging.py
@@ -45,12 +45,12 @@ def configure_logging() -> None:
 
     structlog.configure(
         processors=[
+            structlog.contextvars.merge_contextvars,
             structlog.processors.TimeStamper(fmt='iso'),
             structlog.stdlib.add_log_level,
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
             structlog.processors.JSONRenderer(),
-            structlog.contextvars.merge_contextvars,
         ],
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
